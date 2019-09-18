8faadfd88a452c66e734505bf7ede638ddf9c184
Juanru <yz5026@nyu.edu>
2019-09-18 11:04:25 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+<div>
+<h1>I'm the cutest cat in the world</h1>
+ <img 
+  alt="Cute Cat"
+ src="https://static.boredpanda.com/blog/wp-content/uploads/2018/04/handicapped-cat-rexie-the-handicat-dasha-minaeva-60-5acb4f1d1cdbb__700.jpg"/>
+ 
+<h1>
+  Seriously.
+</h1>
+ <img alt="Cute？Cat" src="https://img.izismile.com/img/img11/20180410/640/this_cat_is_taking_over_the_internet_with_his_hilarious_facial_expressions_despite_his_problem_640_10.jpg"/>
+
+</div>
+
+);
+}
+
+export default App;
